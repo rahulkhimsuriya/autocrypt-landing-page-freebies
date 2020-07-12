0db603e83882d845954e06f2e7b634041d1ecbc5
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2020-07-12 20:23:09 +0530

    change title and description

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 return [
     'production' => false,
     'baseUrl' => '',
-    'title' => 'Jigsaw',
-    'description' => 'Website description.',
+    'title' => 'Autocryp',
+    'description' => 'Autocryp is the place to buy and sell Bitcoin, Ethereum and various kinds of Digital Assets in Indonesia.',
     'collections' => [],
 ];
